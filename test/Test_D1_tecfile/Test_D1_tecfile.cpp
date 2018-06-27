f1f369c0ed65c91a72fc4743ccdd12072b617e67
luanmy <luan_ming_yi@126.com>
2018-06-27 16:54:57 +0800

    add read_tec function

--- test/Test_D1_tecfile/Test_D1_tecfile.cpp
+++ test/Test_D1_tecfile/Test_D1_tecfile.cpp
@@ -5,10 +5,7 @@
 using namespace std;
 #include "../../scr/liton_cpp_snippets/lion_snippets.hpp"
 #include "../../scr/liton_ordered_tec/ordered_tec.h"
-
-#ifdef _DEBUG
-#define _CHECK_POINTDATA_RANGE
-#endif
+#define PD_OT
 #include "../../scr/liton_point_data/PointData.hpp"
 
 using namespace liton_pd;
@@ -40,18 +37,30 @@ int main(int argc, char** argv)
 		tecfile.Zones[0].Data.push_back(TEC_DATA(y.data_pt(0)));
 		tecfile.Zones[0].Data.push_back(TEC_DATA(y.data_pt(1)));
 
-		D1::PD_For_1D(x.size().range(RA::ALL), [&]PD_F_i(i) {
+		D1::PD_For_1D(x.size().range(RA::ALL), [&]PD_F_i(i)
+		{
 			x(0, i, FL::C) = static_cast<double>(i) / (x.size().size(0, RA::IN) - 1);
 			y(0, i, FL::C) = x(0, i, FL::C) / 2;
-			y(1, i, FL::C) = x(0, i, FL::C)*x(0, i, FL::C);
+			y(1, i, FL::C) = x(0, i, FL::C) * x(0, i, FL::C);
 		});
 
 		tecfile.write_plt();
 		tecfile.last_log.write_echo(out);
+		tecfile.last_log.write_xml();
+
+		D1::PointData<double, 3, LO::center> z(0, 100, 0);
+		z.read_plt(".", tecfile.last_log, 0, "x", 0);
+		z.read_plt(".", tecfile.last_log, 0, "y1", 1);
+		z.read_plt(".", tecfile.last_log, 0, "y2", 2);
+		out << z.disp_data() << endl;
+
+		liton_sp::debug::exec_except([&]() {z.read_plt(".", tecfile.last_log, 0, "y2", 3);}, out, out);
+		liton_sp::debug::exec_except([&]() {z.read_plt(".", tecfile.last_log, 0, "xxx", 0);}, out, out);
+		liton_sp::debug::exec_except([&]() {z.read_plt("aaa", tecfile.last_log, 0, "y2", 0);}, out, out);
 
 		out.close();
 	}
-	catch (const std::exception& err)
+	catch (const std::exception &err)
 	{
 		cout << err.what() << endl;
 	}
